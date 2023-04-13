00dea02ff8640da7e4e4c67ffeac21a1df717c0b
WaterDeez <water997@pm.me>
2023-04-13 16:08:38 +1000

    initial install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "This script is intended for a proxmox container, and thus run as root."
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+apt update
+apt -y upgrade
+apt install -y python python3-pip git curl
+pip install requires discord.py
+git clone https://github.com/WaterDeez/IceBerg
+cd IceBerg
+echo "Edit config.json, then run: python3 iceberg.py"
+
